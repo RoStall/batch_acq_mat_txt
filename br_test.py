9a51850af8262933065d11fa2499f5d51b45a22f
RoStall <robert.stallard@louisville.edu>
2015-01-19 22:38:09 -0500

    biopac data format test
    Basically a learning exercise to see how this package fits together.

---
+++ br_test.py
@@ -0,0 +1,9 @@
+__author__ = 'Robert Stallard'
+
+# TODO test alterations of TxtWriter class toward goal of combining columns into single file.
+
+import bioread
+import os
+import glob
+
+directory = 'E:\Dropbox\BedRestStudy\MVC_PRE_2-2_test'
